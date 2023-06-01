38ef1ea67aff9c79151c9c29c07d45aad501d3de
achylikina <a.chilikina@hotmail.com>
2023-06-01 14:19:11 -0400

    MessageService added. RxJS of() added

--- TourOfHeroes/src/app/heroes/heroes.component.ts
+++ TourOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HeroService} from '../hero.service';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 
 @Component({
@@ -9,9 +10,9 @@ import { HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   heroes: Hero[] = [];
   selectedHero?: Hero;
@@ -22,10 +23,11 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-    .subscribe(s_heroes => this.heroes = s_heroes);
+      .subscribe(s_heroes => this.heroes = s_heroes);
   }
 }
